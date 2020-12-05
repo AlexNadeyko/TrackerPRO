0f30ae45c1742145a52481625bb32594ba249145
Alex Nadzeika <nadeykoa@gmail.com>
2020-12-05 17:50:54 +0100

    Adding /app/build to .gitignore file

--- app/src/main/java/com/example/trackerpro/SplashActivity.kt
+++ app/src/main/java/com/example/trackerpro/SplashActivity.kt
@@ -7,5 +7,8 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+//        Thread.sleep(10000)
+//
+//        setContentView(R.layout.activity_tracking)
     }
 }
